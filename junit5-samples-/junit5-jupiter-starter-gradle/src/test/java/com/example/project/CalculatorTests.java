90f29be51c1a01e4406910e99ca5ef5d3c24017d
andreyrehv <andreycaldas@gmail.com>
2019-08-23 08:55:44 -0300

    Teste

---
+++ junit5-samples-/junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CalculatorTests {
+	
+	Calculator calculator = new Calculator();
+	@Test
+    void addition() {
+        assertEquals(2, calculator.soma1(1, 1));
+    }
+	void addition() {
+        assertEquals(2, calculator.soma2(1.5, 1.5));
+    }
+
+}	
